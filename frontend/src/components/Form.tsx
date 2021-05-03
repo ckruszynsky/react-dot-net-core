fe86dc6c1f027be89d0f8508927ff56016ffb239
Chad Kruszynsky <ckruszynsky@gmail.com>
2021-05-03 16:06:33 -0500

    add form validation

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { FC, useState,createContext } from 'react';
+import { FC, useState, createContext } from 'react';
 import { PrimaryButton, gray5, gray6 } from '../styles';
 /** @jsxRuntime classic */
 /** @jsx jsx */
@@ -7,52 +7,126 @@ import { css, jsx } from '@emotion/react';
 export interface Values {
   [key: string]: any;
 }
+export interface Errors {
+  [key: string]: string[];
+}
 
+export interface Touched {
+  [key: string]: boolean;
+}
+
+interface Validation {
+  validator: Validator;
+  arg?: any;
+}
+interface ValidationProp {
+  [key: string]: Validation | Validation[];
+}
 interface Props {
   submitCaption?: string;
+  validationRules?: ValidationProp;
 }
 
 interface FormContextProps {
   values: Values;
-  setValue?: (fieldName: string, value: any) => void; 
+  setValue?: (fieldName: string, value: any) => void;
+  errors: Errors;
+  validate?: (fieldName: string) => void;
+  touched: Touched;
+  setTouched?: (fieldName: string) => void;
 }
 
-export const FormContext = createContext<FormContextProps>({values: {}});
+export const FormContext = createContext<FormContextProps>({
+  values: {},
+  errors: {},
+  touched: {},
+});
 
-export const Form: FC<Props> = ({ submitCaption, children }) => {
+type Validator = (value: any, args?: any) => string;
+
+export const required: Validator = (value: any): string =>
+  value === undefined || value === null || value === ''
+    ? 'This must be populated'
+    : '';
+
+export const minLength: Validator = (value: any, length: number): string =>
+  value && value.length < length
+    ? `This must be at least ${length} characters`
+    : '';
+
+export const Form: FC<Props> = ({
+  submitCaption,
+  children,
+  validationRules,
+}) => {
   const [values, setValues] = useState<Values>({});
+  const [errors, setErrors] = useState<Errors>({});
+  const [touched, setTouched] = useState<Touched>({});
+
+  const validate = (fieldName: string): string[] => {
+    if (!validationRules) {
+      return [];
+    }
+    if (!validationRules[fieldName]) {
+      return [];
+    }
+    const rules = Array.isArray(validationRules[fieldName])
+      ? (validationRules[fieldName] as Validation[])
+      : ([validationRules[fieldName]] as Validation[]);
+
+    const fieldErrors: string[] = [];
+
+    rules.forEach((rule) => {
+      const error = rule.validator(values[fieldName], rule.arg);
+      if (error) {
+        fieldErrors.push(error);
+      }
+    });
+
+    const newErrors = { ...errors, [fieldName]: fieldErrors };
+    setErrors(newErrors);
+    return fieldErrors;
+  };
+
   return (
     <FormContext.Provider
       value={{
         values,
-        setValue: (fieldName:string, value:any) => {
-          setValues({...values, [fieldName]: value})
-        }
-      }}>
-    <form noValidate={true}>
-      <fieldset
-        css={css`
-          margin-top: 10px auto 0 auto;
-          padding: 30px;
-          width: 500px;
-          background-color: ${gray6};
-          border-radius: 4px;
-          border: 1px solid ${gray5};
-          box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
-        `}
-      >
-        {children}
-        <div
+        setValue: (fieldName: string, value: any) => {
+          setValues({ ...values, [fieldName]: value });
+        },
+        errors,
+        validate,
+        touched,
+        setTouched: (fieldName: string) => {
+          setTouched({ ...touched, [fieldName]: true });
+        },
+      }}
+    >
+      <form noValidate={true}>
+        <fieldset
           css={css`
-            margin: 30px 0px 0px 0px;
-            padding: 20px 0px 0px 0px;
-            border-top: 1px solid ${gray5};
+            margin-top: 10px auto 0 auto;
+            padding: 30px;
+            width: 500px;
+            background-color: ${gray6};
+            border-radius: 4px;
+            border: 1px solid ${gray5};
+            box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
           `}
         >
-          <PrimaryButton type="submit">{submitCaption}</PrimaryButton>
-        </div>
-      </fieldset>
-    </form>
+          {children}
+          <div
+            css={css`
+              margin: 30px 0px 0px 0px;
+              padding: 20px 0px 0px 0px;
+              border-top: 1px solid ${gray5};
+            `}
+          >
+            <PrimaryButton type="submit">{submitCaption}</PrimaryButton>
+          </div>
+        </fieldset>
+      </form>
     </FormContext.Provider>
   );
 };
--- frontend/src/pages/AskPage.tsx
+++ frontend/src/pages/AskPage.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Page } from './Page';
-import { Form } from '../components/Form';
+import { Form, required, minLength } from '../components/Form';
 import { Field } from '../components/Field';
 
 export const AskPage = () => (
   <Page title="Ask a question">
-    <Form submitCaption="Submit Your Question">
+    <Form
+      submitCaption="Submit Your Question"
+      validationRules={{
+        title: [{ validator: required }, { validator: minLength, arg: 10 }],
+        content: [{ validator: required }, { validator: minLength, arg: 50 }],
+      }}
+    >
       <Field name="title" label="Title" />
       <Field name="content" label="Content" type="TextArea" />
     </Form>
