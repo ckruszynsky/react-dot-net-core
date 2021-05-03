acc0f7a9bc2be23992826e59ce2aa94454998bf5
Chad Kruszynsky <ckruszynsky@gmail.com>
2021-05-03 14:17:18 -0500

    add lazy loading ask component with suspense

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,18 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react';
-import React, {FC} from 'react';
+import React, { FC, lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-import { AskPage } from './pages/AskPage';
 import { SearchPage } from './pages/SearchPage';
 import { SignInPage } from './pages/SignInPage';
-import {PageNotFound} from './pages/PageNotFound';
+import { PageNotFound } from './pages/PageNotFound';
 import { HomePage } from './pages/HomePage';
 
-import { Header } from './components/Header';
+import { HeaderWithRouter as Header } from './components/Header';
 import { fontFamily, fontSize, gray2 } from './styles';
 import { QuestionPage } from './pages/QuestionPage';
+
+const AskPage = lazy(() => import('./pages/AskPage'));
 
 const App: FC = () => {
   return (
@@ -23,12 +24,25 @@ const App: FC = () => {
           color: ${gray2};
         `}
       >
-        <Header appName="Q & A" />
+        <Header />
         <Switch>
           <Redirect from="/home" to="/" />
           <Route exact path="/" component={HomePage} />
           <Route path="/search" component={SearchPage} />
-          <Route path="/ask" component={AskPage} />
+          <Route path="/ask">
+            <Suspense
+              fallback={
+                <div
+                  css={css`
+                    margin-top: 100px;
+                    text-align: center;
+                  `}
+                >
+                  Loading....
+                </div>
+              }
+            ></Suspense>
+          </Route>
           <Route path="/signin" component={SignInPage} />
           <Route path="/questions/:questionId" component={QuestionPage} />
           <Route component={PageNotFound} />
